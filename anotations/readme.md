b92d94f8f4646adc2c702e244be0cd0efa8bd9e4
eliseu441 <eliseu441@gmail.com>
2024-06-24 13:54:27 -0300

    v0.1

---
+++ anotations/readme.md
@@ -0,0 +1,5 @@
+instalar no ubuntu
+sudo apt-get install libssl-dev
+
+necessario para rodar "sqlx migrate add -r init"
+cargo install sqlx-cli
---
+++ src/schema.rs
@@ -0,0 +1,8 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+
+pub struct CreateTaskSchema {
+    pub title: String,
+    pub content: String,
+}
